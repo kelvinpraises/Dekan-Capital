fefd62982f38d2a2f8cde37ee328cb03d4320209
kelvinpraises <kelvinpraises@gmail.com>
2024-01-18 09:16:23 +0100

    feat: add root provider

--- library/components/template/LayoutWrapper.tsx
+++ library/components/template/LayoutWrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Inter } from "next/font/google";
 
+import RootProvider from "@/library/providers/provider";
 import Footer from "../organisms/Footer";
 import Header from "../organisms/Header";
 
@@ -10,11 +11,13 @@ const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className=" flex flex-col gap-1 w-screen relative h-screen">
-          <Header />
-          <div className=" flex px-1 gap-1 h-full">{children}</div>
-          <Footer className=" pt-8" />
-        </main>
+        <RootProvider>
+          <main className=" flex flex-col gap-1 w-screen relative h-screen">
+            <Header />
+            <div className=" flex px-1 gap-1 h-full">{children}</div>
+            <Footer className=" pt-8" />
+          </main>
+        </RootProvider>
       </body>
     </html>
   );
