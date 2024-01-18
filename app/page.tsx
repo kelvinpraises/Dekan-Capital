ead77bb4b39ee0bd3a3a23a85ff77450e2cda181
kelvinpraises <kelvinpraises@gmail.com>
2024-01-18 08:28:45 +0100

    chore: remove prev home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Button from '@/library/components/atoms/Button'
-import Image from 'next/image'
+import React from "react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button text={'Sign in'} handleClick={undefined} />
-    </main>
-  )
-}
+const page = () => {
+  return <div>home page</div>;
+};
+
+export default page;
