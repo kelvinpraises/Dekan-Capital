5ccda1ebe835e89aaf40adf2252f0407f8b3821e
kelvinpraises <kelvinpraises@gmail.com>
2024-01-18 15:04:01 +0100

    feat: init zustand store

---
+++ library/store/createGlobalAppStateSlice.ts
@@ -0,0 +1,72 @@
+import { immer } from "zustand/middleware/immer";
+
+type State = {
+  appActive: boolean;
+  userAddress: string;
+  userName: string;
+  userAvatarUrl: string;
+  modalStep: {
+    status: string;
+  }[];
+  txHashes: {
+    [key: string]: string;
+  };
+};
+
+type Actions = {
+  setAppActive: (isActive: boolean) => void;
+  setUserAddress: (address: string) => void;
+  setUserName: (name: string) => void;
+  setUserAvatarUrl: (avatarUrl: string) => void;
+  setModalStep: (modalStep: { status: string }[]) => void;
+  setModalStepIndex: (index: number, item: { status: string }) => void;
+  setTransactionHashes: (txHashes: { [key: string]: string }) => void;
+};
+
+export default immer<State & Actions>((set, get) => ({
+  appActive: false,
+  userAddress: "",
+  userName: "",
+  userAvatarUrl: "",
+  modalStep: [],
+  txHashes: {},
+
+  setAppActive: (isActive) =>
+    set((state) => {
+      state.appActive = isActive;
+    }),
+
+  setUserAddress: (address) =>
+    set((state) => {
+      state.userAddress = address;
+    }),
+
+  setUserName: (name) =>
+    set((state) => {
+      state.userName = name;
+    }),
+
+  setUserAvatarUrl: (avatarUrl) =>
+    set((state) => {
+      state.userAvatarUrl = avatarUrl;
+    }),
+
+  setModalStep: (modalStep) =>
+    set((state) => {
+      state.modalStep = modalStep;
+    }),
+
+  setModalStepIndex: (index, item) => {
+    const newSteps = [...get().modalStep];
+    newSteps[index] = item;
+
+    set((state) => {
+      state.modalStep = newSteps;
+    });
+  },
+
+  setTransactionHashes: (txHashes) =>
+    set((state) => {
+      state.txHashes = txHashes;
+    }),
+}));
