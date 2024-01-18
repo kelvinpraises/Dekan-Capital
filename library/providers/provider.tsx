68fdbcb32969061bf41c2534cc22e1d7b0003675
kelvinpraises <kelvinpraises@gmail.com>
2024-01-18 08:53:33 +0100

    feat: add connect kit provider

---
+++ library/providers/provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import ConnectKitProvider from "./connectkit";
+import WagmiProvider from "./wagmi";
+
+const RootProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-full">
+      <WagmiProvider>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </WagmiProvider>
+    </div>
+  );
+};
+
+export default RootProvider;
