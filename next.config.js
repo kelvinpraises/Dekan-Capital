7692fcd94fbef1a2d409335b5a40163cac728d14
kelvinpraises <kelvinpraises@gmail.com>
2024-01-18 10:10:46 +0100

    feat: support Node.js dependencies

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
