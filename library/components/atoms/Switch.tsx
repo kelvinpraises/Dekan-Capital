89a6daeabc6f611e9d634f1793c0d0c7abf89a07
kelvinpraises <kelvinpraises@gmail.com>
2024-01-20 08:13:22 +0100

    fix: increase bg opacity

--- library/components/atoms/Switch.tsx
+++ library/components/atoms/Switch.tsx
@@ -18,7 +18,7 @@ const Switch = (prop: SwitchProps) => {
       </span>
       <div className="flex gap-4 items-center">
         <_Switch.Root
-          className="w-[42px] h-[25px] bg-gray-300 rounded-full relative shadow-md focus:outline-none cursor-pointer data-[state=checked]:bg-[#63392c]"
+          className="w-[42px] h-[25px] bg-gray-300 rounded-full relative shadow-md focus:outline-none cursor-pointer data-[state=checked]:bg-[#63392c]/80"
           id="toggle-switch"
         >
           <_Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full shadow-md transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
