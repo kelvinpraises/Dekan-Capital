12587d36953ab776d92b816f07d0e7232ce08404
kelvinpraises <kelvinpraises@gmail.com>
2024-01-20 01:39:30 +0100

    feat: init editor js tools

---
+++ library/utils/tools.js
@@ -0,0 +1,30 @@
+import CheckList from "@editorjs/checklist";
+import Code from "@editorjs/code";
+import Delimiter from "@editorjs/delimiter";
+import Header from "@editorjs/header";
+import Link from "@editorjs/link";
+import List from "@editorjs/list";
+import Paragraph from "@editorjs/paragraph";
+import Image from "@editorjs/image";
+import Quote from "@editorjs/quote";
+
+export const EDITOR_JS_TOOLS = {
+  code: Code,
+  paragraph: {
+    class: Paragraph,
+    inlineToolbar: true,
+  },
+  checkList: CheckList,
+  list: List,
+  header: Header,
+  delimiter: Delimiter,
+  quotes: Quote,
+  image: Image,
+  link: {
+    class: Link,
+    config: {
+      endpoint: "http://127.0.0.1:3002/fetchUrl", // Your backend endpoint for url data fetching,
+    },
+  },
+};
+
