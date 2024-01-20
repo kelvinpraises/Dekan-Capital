f2bfbb3c082832625080607b0f9b787b889e639f
kelvinpraises <kelvinpraises@gmail.com>
2024-01-20 08:09:27 +0100

    feat: add radix switch

--- library/components/atoms/Switch.tsx
+++ library/components/atoms/Switch.tsx
@@ -1,3 +1,5 @@
+import * as _Switch from "@radix-ui/react-switch";
+
 interface SwitchProps {
   label?: string;
   description: string;
@@ -14,8 +16,14 @@ const Switch = (prop: SwitchProps) => {
         {prop.label && <p className=" text-sm font-bold">{prop.label}</p>}
         {prop.optional && <p className=" text-sm font-normal">(Optional)</p>}
       </span>
-      <div className="flex gap-5">
-        <p className=" text-sm font-normal">{prop.description}</p>
+      <div className="flex gap-4 items-center">
+        <_Switch.Root
+          className="w-[42px] h-[25px] bg-gray-300 rounded-full relative shadow-md focus:outline-none cursor-pointer data-[state=checked]:bg-[#63392c]"
+          id="toggle-switch"
+        >
+          <_Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full shadow-md transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+        </_Switch.Root>
+        <p className="text-sm font-normal">{prop.description}</p>
       </div>
     </div>
   );
