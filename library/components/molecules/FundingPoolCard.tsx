29364ed00da2456b08b9f180263091b4729a1b1c
kelvinpraises <kelvinpraises@gmail.com>
2024-01-21 16:35:59 +0100

    chore: make dynamic

--- library/components/molecules/FundingPoolCard.tsx
+++ library/components/molecules/FundingPoolCard.tsx
@@ -2,31 +2,37 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface IFundingPoolCard {
+  avatarURL: string;
+  name: string;
   title: string;
-  href: string;
-  imgsrc: string;
-  closing: string;
-  open: boolean;
+  poolFundId: number;
+  active: boolean;
+  poolSize: number;
 }
 
-const FundingPoolCard = (card: IFundingPoolCard) => {
+const FundingPoolCard = (prop: IFundingPoolCard) => {
   return (
     <Link
-      className=" flex justify-between items-center w-full p-4 hover:bg-slate-200 rounded-md"
-      href={card.href}
+      className="flex justify-between items-center p-4 hover:bg-slate-200 rounded-md"
+      href={`/pools/${prop.poolFundId}`}
     >
-      <div className=" flex gap-4 items-center h-[64px]">
-        <Image src={card.imgsrc} alt={card.title} width={64} height={64} />
-        <div className=" flex flex-col justify-between h-full">
-          <p>{card.title}</p>
-          <p className=" text-sm text-[#767b82] ">Superteam uk</p>
+      <div className="flex gap-4">
+        <Image
+          src={prop.avatarURL ? prop.avatarURL : "/pic.svg"}
+          alt={prop.title}
+          width={64}
+          height={64}
+        />
+        <div className=" flex flex-col">
+          <p className="truncate max-w-[20ch]">{prop.title}</p>
+          <p className=" text-sm text-[#767b82]">{prop.name}</p>
           <div className=" flex gap-8 items-center font-light">
-            {card.closing && (
+            {/* {card.closing && (
               <p className=" text-xs text-[#767b82] border-r border-r-[#e4e5e6] pr-8">
                 closing in {card.closing} days
               </p>
-            )}
-            {card.open ? (
+            )} */}
+            {prop.active ? (
               <p className=" text-green-500 text-xs">open</p>
             ) : (
               <p className=" text-pink-600 text-xs">closed</p>
@@ -34,7 +40,7 @@ const FundingPoolCard = (card: IFundingPoolCard) => {
           </div>
         </div>
       </div>
-      <p className=" text-lg font-medium">550 USDC</p>
+      <p className="text-lg font-medium min">{prop.poolSize} DKN</p>
     </Link>
   );
 };
