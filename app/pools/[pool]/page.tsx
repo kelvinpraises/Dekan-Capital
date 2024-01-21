01045c83eb56e488eea6b5fff3b5169ebcd9d431
kelvinpraises <kelvinpraises@gmail.com>
2024-01-21 16:36:58 +0100

    feat: add params

--- app/pools/[pool]/page.tsx
+++ app/pools/[pool]/page.tsx
@@ -1,16 +1,19 @@
-"use client"
+"use client";
 
-import PoolHeader from '@/library/components/molecules/PoolHeader'
-import PoolNav from '@/library/components/molecules/PoolNav'
-import React from 'react'
+import { useParams } from "next/navigation";
+
+import PoolNav from "@/library/components/molecules/PoolBody";
+import PoolHeader from "@/library/components/molecules/PoolHeader";
 
 const page = () => {
-  return (
-    <div className=' flex-1'>
-      <PoolHeader />
-      <PoolNav />
-    </div>
-  )
-}
+  const { pool: poolFundId } = useParams();
 
-export default page
+  return (
+    <div className=" flex-1">
+      <PoolHeader poolFundId={poolFundId as string} />
+      <PoolNav poolFundId={poolFundId as string} />
+    </div>
+  );
+};
+
+export default page;
