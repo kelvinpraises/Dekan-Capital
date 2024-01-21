d5a23cafa79c9390f7f941890b6ecf81ebff61ac
kelvinpraises <kelvinpraises@gmail.com>
2024-01-21 13:57:14 +0100

    chore: add new tabs and rename

--- library/components/molecules/PoolNav.tsx
+++ library/components/molecules/PoolNav.tsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 import AllocationSection from "../organisms/AllocationSection";
 import DetailSection from "../organisms/DetailSection";
 import DistributionSection from "../organisms/DistributionSection";
-import ProjectSection from "../organisms/ProjectSection";
+import ReviewSection from "../organisms/ReviewSection";
 import SubmitSection from "../organisms/SubmitSection";
 import VotingSection from "../organisms/VotingSection";
 import { usePathname } from "next/navigation";
+import ShowcaseSection from "../organisms/ShowcaseSection";
 
 const PoolNav = () => {
   const [activeScreen, setActiveScreen] = useState("detail");
@@ -14,7 +15,7 @@ const PoolNav = () => {
   const id = pathname.split("/")[3];
 
   return (
-    <div className="  bg-[#f4f9ff] flex-1 h-full">
+    <div className="  bg-[#F3EFEE] flex-1 h-full">
       <div className="flex gap-4 items-center text-[#abbfdb] py-3 bg-white px-16">
         <button
           className={cn(activeScreen === "detail" && "text-[#334254]")}
@@ -29,22 +30,28 @@ const PoolNav = () => {
           Projects
         </button>
         <button
-          className={cn(activeScreen === "allocation" && "text-[#334254]")}
-          onClick={() => setActiveScreen("allocation")}
+          className={cn(activeScreen === "review" && "text-[#334254]")}
+          onClick={() => setActiveScreen("review")}
         >
-          Allocation
+          Review
         </button>
         <button
-          className={cn(activeScreen === "voting" && "text-[#334254]")}
-          onClick={() => setActiveScreen("voting")}
+          className={cn(activeScreen === "allocate" && "text-[#334254]")}
+          onClick={() => setActiveScreen("allocate")}
         >
-          Voting
+          Allocate
         </button>
         <button
-          className={cn(activeScreen === "distribution" && "text-[#334254]")}
-          onClick={() => setActiveScreen("distribution")}
+          className={cn(activeScreen === "vote" && "text-[#334254]")}
+          onClick={() => setActiveScreen("vote")}
         >
-          Distribution
+          Vote
+        </button>
+        <button
+          className={cn(activeScreen === "distribute" && "text-[#334254]")}
+          onClick={() => setActiveScreen("distribute")}
+        >
+          Distribute
         </button>
       </div>
 
@@ -57,14 +64,16 @@ const PoolNav = () => {
                   <DetailSection /> <SubmitSection />
                 </div>
               );
-            case "project":
-              return <ProjectSection ecoFundId={id} />;
-            case "allocation":
-              return <AllocationSection ecoFundId={id} />;
-            case "voting":
-              return <VotingSection ecoFundId={id} />;
-            case "distribution":
-              return <DistributionSection ecoFundId={id} />;
+            case "showcase":
+              return <ShowcaseSection poolFundId={id} />;
+            case "review":
+              return <ReviewSection poolFundId={id} />;
+            case "allocate":
+              return <AllocationSection poolFundId={id} />;
+            case "vote":
+              return <VotingSection poolFundId={id} />;
+            case "distribute":
+              return <DistributionSection poolFundId={id} />;
             default:
               return null;
           }
