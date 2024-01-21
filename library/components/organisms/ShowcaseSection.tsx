a0be2e88a8022b6da1c2b7efcfd0099868e474c8
kelvinpraises <kelvinpraises@gmail.com>
2024-01-21 13:56:13 +0100

    feat: implement showcase section

--- library/components/organisms/ShowcaseSection.tsx
+++ library/components/organisms/ShowcaseSection.tsx
@@ -1,8 +1,60 @@
+import { useEffect, useReducer } from "react";
 
-const DetailSection = () => {
-  return (
-    <div className=" flex-1 bg-white p-5 rounded-lg">showcase section</div>
-  )
+import { getPoolFundProjects } from "@/library/services/backendAPI";
+import LargeCard from "../molecules/LargeCard";
+
+interface PoolFundProject {
+  projectId: number;
+  createdBy: string;
+  tokensRequested: number;
+  emoji: string;
+  title: string;
+  description: string;
 }
 
-export default DetailSection
+type PoolFundProjects = { poolFundProjects: PoolFundProject[] };
+
+const initialState = {
+  poolFundProjects: [],
+};
+
+const stateReducer = (
+  current: PoolFundProjects,
+  update: Partial<PoolFundProjects>
+): PoolFundProjects => {
+  return {
+    ...current,
+    ...update,
+  };
+};
+
+const ShowcaseSection = ({ poolFundId }: { poolFundId: string }) => {
+  const [values, updateValues] = useReducer(stateReducer, initialState);
+
+  useEffect(() => {
+    (async () => {
+      const poolFundProjects = await getPoolFundProjects(poolFundId);
+      updateValues({ poolFundProjects });
+    })();
+  }, []);
+
+  return (
+    <div className="w-[520px] flex flex-col gap-4">
+      {values.poolFundProjects?.map((x) => {
+        return (
+          <LargeCard
+            title={x.title}
+            description={x.description}
+            buttonText={"Open Fund"}
+            buttonImg={"enter.svg"}
+            buttonOnclick={() => {
+              throw new Error("Function not implemented.");
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default ShowcaseSection;
